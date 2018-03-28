dfb74998857af593c86859a214d19cb7e74a1db1
Hrayr Harutyunyan <harhro@gmail.com>
2018-03-28 15:43:19 +0400

    Re-train some models for m=4,bs=32,ns=32 case

--- baselines.py
+++ baselines.py
@@ -424,3 +424,93 @@ class TCorex(Baseline):
         finish_time = time.time()
         print("\tElapsed time {:.1f}s, mean score = {}".format(finish_time - start_time, np.mean(scores)))
         return self.report_scores(scores, n_iter)
+
+
+""" The following method doesn't train new T-Corex for testing, it just saves the covariance estimate
+of the best model and then calculates the scores on the test set.
+
+class TCorex(Baseline):
+    def __init__(self, tcorex, **kwargs):
+        self.tcorex = tcorex
+        super(TCorex, self).__init__(**kwargs)
+
+    def select(self, train_data, val_data, params):
+        print("Selecting the best parameter values for {} ...".format(self.name))
+        best_score = 1e18
+        best_params = None
+        self.best_covs = None
+
+        const_params = dict()
+        search_params = []
+        for k, v in params.items():
+            if k in ['l1', 'l2']:
+                continue
+            if isinstance(v, list):
+                arr = [(k, x) for x in v]
+                search_params.append(arr)
+            else:
+                const_params[k] = v
+
+        # add L1/L2 regularization parameters
+        reg_params = []
+        if 'l1' in params:
+            reg_params += [('l1', x) for x in params['l1']]
+        if 'l2' in params:
+            reg_params += [('l2', x) for x in params['l2']]
+        if len(reg_params) > 0:
+            search_params.append(reg_params)
+
+        # add a dummy variable if the grid is empty
+        if len(search_params) == 0:
+            search_params = [[('dummy', None)]]
+
+        grid = list(itertools.product(*search_params))
+
+        for index, cur_params in enumerate(grid):
+            print("done {} / {}".format(index, len(grid)), end='')
+            print(" | running with ", end='')
+            for k, v in cur_params:
+                print('{}: {}\t'.format(k, v), end='')
+            print('')
+
+            cur_params = dict(cur_params)
+            for k, v in const_params.items():
+                cur_params[k] = v
+            cur_params['nt'] = len(train_data)
+
+            cur_score, covs = self.evaluate(train_data, val_data, cur_params,
+                                            n_iter=1, verbose=False, validation=True)
+            cur_score = cur_score['mean']
+
+            if (best_params is None) or (not np.isnan(cur_score) and cur_score < best_score):
+                best_score = cur_score
+                best_params = cur_params
+                self.best_covs = covs
+
+        print('\n')
+        return (best_params, best_score)
+
+    def evaluate(self, train_data, test_data, params, n_iter, verbose=True, validation=False):
+        if verbose:
+            print("Evaluating {} for {} iterations ...".format(self.name, n_iter))
+        start_time = time.time()
+
+        scores = []
+        covs = None
+        if validation:
+            assert n_iter == 1
+            for iteration in range(n_iter):
+                c = self.tcorex(**params)
+                c.fit(train_data)
+                covs = c.get_covariance()
+                cur_nll = metric_utils.calculate_nll_score(data=test_data, covs=covs)
+                scores.append(cur_nll)
+        else:
+            scores = [metric_utils.calculate_nll_score(data=test_data, covs=self.best_covs)] * n_iter
+        finish_time = time.time()
+        print("\tElapsed time {:.1f}s, mean score = {}".format(finish_time - start_time, np.mean(scores)))
+        ret = self.report_scores(scores, n_iter)
+        if validation:
+            ret = (ret, covs)
+        return ret
+"""
