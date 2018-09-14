68304336f529770008278092aabcfae970fa3d5f
Hrayr Harutyunyan <harhro@gmail.com>
2018-09-15 02:04:53 +0400

    Do some rearraning of files; Merge all util files

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,11 +1,26 @@
-from __future__ import division
+from __future__ import print_function
 from __future__ import absolute_import
 
-import os
+from scipy.stats import multivariate_normal
 import numpy as np
+import os
+
+
+def calculate_nll_score(data, covs):
+    """ Calculate negative log-likelihood of covariance estimates under data.
+    """
+    nt = len(data)
+    try:
+        nll = [-np.mean([multivariate_normal.logpdf(sx, cov=covs[t]) for sx in x])
+               for x, t in zip(data, range(nt))]
+    except Exception:
+        nll = [np.inf]
+    return np.mean(nll)
 
 
 def make_buckets(ts_data, test_data, window, stride):
+    """ Divide timeseries data into buckets.
+    """
     nt = len(ts_data)
     K = 2 * window + 1
 
@@ -52,3 +67,27 @@ def make_sure_path_exists(path):
         os.makedirs(dir_name)
     except:
         pass
+
+
+def plot_cov_matrix(plt, cov, title=None, vmin=-1, vmax=+1):
+    plt.figure(figsize=(8, 8))
+    plt.title(title)
+    plt.imshow(cov, vmin=vmin, vmax=vmax)
+    plt.colorbar()
+    plt.show()
+
+
+def plot_for_next_timestep(plt, data, covs,title="Negative log-likelihood of estimate of time step $t$ under "
+                                                 "the test data of timestep $t + 1$"):
+    nt = len(data)
+    nll = [-np.mean([multivariate_normal.logpdf(sx, cov=covs[t]) for sx in x])
+           for x, t in zip(data[1:], range(nt-1))]
+    plt.bar(range(1, nt), nll, width=0.6)
+    plt.title(title)
+    plt.xlabel("Timestep")
+    plt.ylim(0)
+    plt.ylabel("Negative log-likelihood")
+    plt.xticks(range(1, nt))
+    plt.show()
+    print("NLL for next time step = {}".format(np.mean(nll)))
+    return np.mean(nll)
