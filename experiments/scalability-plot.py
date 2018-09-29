293d78bfbbb67085f23004a43ab10344f03c73c2
Hrayr Harutyunyan <harhro@gmail.com>
2018-09-29 04:01:22 +0400

    scalability is finalized

--- experiments/scalability-plot.py
+++ experiments/scalability-plot.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-from experiments.generate_data import load_sudden_change
+from experiments.generate_data import load_nglf_sudden_change
 from experiments import baselines
+from experiments.utils import make_sure_path_exists
 import theano_tcorex
 import pytorch_tcorex
 import argparse
@@ -12,11 +13,8 @@ import json
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--nt', type=int, default=10, help='number of buckets')
-    parser.add_argument('--m', type=int, default=8, help='number of latent factors')
     parser.add_argument('--train_cnt', default=16, type=int, help='number of train samples')
-    parser.add_argument('--min_std', type=float, default=0.25, help='minimum x-std')
-    parser.add_argument('--max_std', type=float, default=4.0, help='maximum x-std')
-    parser.add_argument('--nvs', type=int, nargs='+', default=[2 ** i for i in range(3, 18)])
+    parser.add_argument('--nvs', type=int, nargs='+', default=[2 ** i for i in range(3, 21)])
     parser.add_argument('--prefix', type=str, default='', help='Additional prefix of out file name')
     args = parser.parse_args()
     print(args)
@@ -37,31 +35,39 @@ def main():
         #     'gamma': 0.8,
         #     'reg_type': 'W',
         #     'init': True,
-        #     'n_hidden': args.m,
         #     'ignore_sigma': True,
         # }),
 
-        # (baselines.TCorex(tcorex=pytorch_tcorex.TCorex, name='T-Corex (pytorch, cpu)'), {
-        #     'max_iter': 100,
-        #     'anneal': True,
-        #     'l1': 0.1,
-        #     'gamma': 0.8,
-        #     'reg_type': 'W',
-        #     'init': True,
-        #     'n_hidden': args.m,
-        #     'torch_device': 'cpu'  # or 'cuda'
-        # }),
-
-        (baselines.QUIC(name='QUIC'), {
-            'lamb': 0.1,
-            'tol': 1e-6,
-            'msg': 1,
-            'max_iter': 100
+        (baselines.TCorex(tcorex=pytorch_tcorex.TCorex, name='T-Corex (pytorch, cpu)'), {
+            'max_iter': 100,
+            'anneal': True,
+            'l1': 0.1,
+            'gamma': 0.8,
+            'reg_type': 'W',
+            'init': True,
+            'torch_device': 'cpu'
         }),
 
-        # (baselines.BigQUIC(name='BigQUIC'), {
+        (baselines.TCorex(tcorex=pytorch_tcorex.TCorex, name='T-Corex (pytorch, cuda)'), {
+            'max_iter': 100,
+            'anneal': True,
+            'l1': 0.1,
+            'gamma': 0.8,
+            'reg_type': 'W',
+            'init': True,
+            'torch_device': 'cuda'
+        }),
+
+        # (baselines.QUIC(name='QUIC'), {
         #     'lamb': 0.1,
         #     'tol': 1e-6,
+        #     'msg': 1,
+        #     'max_iter': 100
+        # }),
+
+        # (baselines.BigQUIC(name='BigQUIC'), {
+        #     'lamb': 3,
+        #     'tol': 1e-3,
         #     'verbose': 1,
         #     'max_iter': 100
         # })
@@ -71,17 +77,19 @@ def main():
     for method, params in methods:
         times[method.name] = []
 
-    out_file = 'resources/{}nt{}.m{}.train_cnt{}.min_std{:.2f}.max_std{:.2f}.json'.format(
-        args.prefix, args.nt, args.m, args.train_cnt, args.min_std, args.max_std)
+    out_file = 'experiments/scalability/{}nt{}.train_cnt{}.json'.format(args.prefix, args.nt, args.train_cnt)
+    make_sure_path_exists(out_file)
     print("Output file path = {}".format(out_file))
 
     stop_methods = set()
 
     for nv in args.nvs:
+        bs = min(nv, 16)
+        n_hidden = nv // bs
+
         # generate data
-        (data, _, _, _) = load_sudden_change(nv=nv, m=args.m, nt=args.nt, train_cnt=args.train_cnt, val_cnt=1,
-                                             test_cnt=1, snr=5.0, min_std=args.min_std, max_std=args.max_std,
-                                             nglf=True, shuffle=False, from_matrix=False)
+        data, _, = load_nglf_sudden_change(nv=nv, m=n_hidden, nt=args.nt, ns=args.train_cnt,
+                                           shuffle=False, from_matrix=False)
 
         for method, params in methods:
             # start timing
@@ -89,6 +97,7 @@ def main():
 
             if method.name.find('T-Corex') != -1:
                 params['nv'] = nv
+                params['n_hidden'] = min(n_hidden, 64)
 
             if method in stop_methods:
                 print("\tskipped")
